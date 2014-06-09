537c54be9958ca0e4c5b7d311c1ab7845c8b8f91
laa-1-yay <laavanye.bahl@gmail.com>
2014-06-09 15:08:58 +0530

    Added Main pg with Instructions, video, question

--- babapredictions/src/main/java/com/lrp/babapredictions/MainActivity.java
+++ babapredictions/src/main/java/com/lrp/babapredictions/MainActivity.java
@@ -1,17 +1,93 @@
 package com.lrp.babapredictions;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 
 public class MainActivity extends Activity {
+
+
+
+    EditText question;
+    TextView txt;
+    Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        question = (EditText) findViewById(R.id.editText);
+        txt = (TextView)findViewById(R.id.textView);
+        final VideoView vv = (VideoView)findViewById(R.id.videoView);
+        vv.setVisibility(vv.INVISIBLE);
+        final String f = "android.resource://" + getPackageName() + "/" + R.raw.video;
+
+        button = (Button) findViewById(R.id.a);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //delay in ms
+                int DELAY = 1700;
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(MainActivity.this, OutputSong.class);
+                        startActivity(intent);
+                    }
+                }, DELAY);
+
+                vv.setVisibility(vv.VISIBLE);
+
+
+                vv.setVideoURI(Uri.parse(f));
+                vv.start();
+
+
+            }
+
+
+        });
+
+
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Baba Instructions !");
+        builder.setMessage("Ask any question of your choice about the future, to be answered in Yes/No ");
+        builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+
+
+
+
     }
 
 
